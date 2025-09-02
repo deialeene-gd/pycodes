cc95a6e1cdcecddb394fb1f4d392f7eadf2cd782
Deialeene Delmendo <you@example.com>
2025-09-02 19:58:43 +0800

    test

--- test.py
+++ test.py
@@ -201,3 +201,26 @@ for i in range(10):
 # print(mylist[::2])
 
 # print(mylist[::-1])
+
+"""while True:
+    row = int(input("Enter first number: "))
+    col = int(input("Enter second number: "))
+    srch = int(input("Enter search number: "))
+
+    if row <= 0 or col <= 0:
+        print("Both numbers must be greater than 0. Exiting... ")
+        break
+
+    for i in range(1, row + 1):
+        for o in range(1, col + 1):
+            product = i * o
+            if product == srch:
+                print(f"[{product:2}]", end=" ")
+            else:
+                print(f"{product:4}", end=" ")
+            # if srch == i * o:
+            #     print(f"[{i * o:2}]", end=" ")
+            # else:
+            #     print(f"{i * o:4}", end=" ")
+        print()
+    print()"""
