5390df9db63b9582b99afd8a3e36d7f5bb885f7f
Deialeene Delmendo <you@example.com>
2025-09-16 18:19:19 +0800

    Homework

---
+++ HW1/dictionary.py
@@ -0,0 +1,77 @@
+cart = {0: "underware",
+        1: "tank top",
+        2: "jacket"}
+
+print("Matrix size: ", len(cart))
+for i in range(len(cart)):
+    print(f"{i}: {cart[i]}")
+
+while True:
+
+    action = input("What would you like to do [A]dd item, [C]hange item, [R]emove item, [D]isplay item, [S]earch item? ").lower()
+
+    sorry = "I'm sorry, not in the cart."
+
+    match action:
+
+        case "a":
+            key = input("Enter key: ")
+            value = input("Enter value: ")
+            if key.isdigit():
+                cart[int(key)] = value
+                print("Item added.")
+            else:
+                print("Invalid key.")
+
+        case "c":
+            key = input("Enter key to search: ")
+            if key.isdigit():
+                keys = int(key)
+                if keys in cart:
+                    new_value = input("Enter new value: ")
+                    cart[keys] = new_value
+                    print("Item updated.")
+                else:
+                    print(f"{sorry}")
+            else:
+                print("Invalid key.")
+
+        case "d":
+            print("Displaying value")
+            print("Key \t Value")
+            for key, value in cart.items():
+                print(f"{key} \t {value}")
+
+        case "r":
+            key = input("Enter key to search:")
+            if key.isdigit():
+                keys = int(key)
+                if keys in cart:
+                    value = cart[keys]
+                    del cart[keys]
+                    print(f"The key {keys} with value {value} has been deleted.")
+                else: 
+                    print(f"{sorry}")
+            else:
+                print("Invalid key.")
+            
+
+        case "s":
+            srch = input("Enter key/item to search: ")
+            if srch in cart.values():
+                print(f"Found {srch} item.")
+            elif srch.isdigit():
+                if int(srch) in cart.keys():
+                    print(f"Found key {srch}")
+                else:
+                    print(f"{sorry}")
+            else:
+                print("Invalid key.")
+
+        case "*":
+            print("Bye.")
+            break
+
+        case _:
+            print("Invalid action. Please choose A, C, D, R, or S.")
+
